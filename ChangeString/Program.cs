82934edcc95ffd04f1eea3afd4c35c53ff3f9ef1
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-06 22:55:02 +0000

    Alterar os caracteres da string solicitados pelo utilizador

---
+++ ChangeString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fraseFinal ="";
+            Console.Write("String: ");
+            string frase = Console.ReadLine();
+
+            Console.Write("\nCaráter: ");
+            string caracter = Console.ReadLine();
+
+            char c = char.Parse(caracter);
+
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if(frase[i] == c)
+                {
+                    fraseFinal += "X";
+                }
+                else
+                {
+                    fraseFinal += frase[i];
+                }
+            }
+
+            Console.Write($"\nSerá impresso: {fraseFinal}");
+        }
+    }
+}
