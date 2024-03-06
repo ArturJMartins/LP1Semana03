89e30b1b1ecb28c0ba5d07ae520b053d51e4a80c
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-06 17:20:41 +0000

    Argumentos na consola maiores que 3 e menores que 6

---
+++ RightSize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RightSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if( arg.Length > 3 && arg.Length <=6 )
+                {
+                    Console.WriteLine(arg);
+                }
+                if( arg.Length > 6)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
