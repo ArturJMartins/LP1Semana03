35f25886faf2768e245b861a9a2cb7f48e5c05d4
Artur Martins <arturjcmartins.dev@gmail.com>
2024-03-06 23:50:45 +0000

    Multiplicar matrizes 2 por 2 por uma 2 por 1

---
+++ ArrayMult/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ArrayMult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float[,] matrixA = {
+            { float.Parse(args[0]), float.Parse(args[1]) },
+            { float.Parse(args[2]), float.Parse(args[3]) }
+            };
+
+            float[] matrixB = {
+            float.Parse(args[4]),
+            float.Parse(args[5])
+            };
+
+            float[] result = new float[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                result[i] += matrixA[i, j] * matrixB[j];
+                }
+            }
+
+            Console.WriteLine($"{result[0]}");
+            Console.WriteLine($"{result[1]}");
+        }
+    }
+}
